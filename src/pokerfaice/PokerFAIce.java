07d7d2355db190e560d002bf7c6ba02e240123e6
Rory Buckley <rory.buckley@ucdconnect.ie>
2017-04-09 11:23:29 +0100

    PokerLingus changed to PokerFAIce, try again

---
+++ src/pokerfaice/PokerFAIce.java
@@ -0,0 +1,15 @@
+package pokerfaice;
+
+/**
+ * Main class for the PokerFAIce Twitter Bot
+ *
+ */
+public class PokerFAIce {
+    /**
+     * Main method for the PokerFAIce Twitter Bot
+     * @param args
+     */
+    public static void main(String[] args){
+        
+    }
+}

92abb88c03c898b19d81097ba8e96d7463bd5dae
Ciaran-Murphy <ciaran.murphy.4@ucdconnect.ie>
2017-04-22 18:07:39 +0100

    Draw Poker Round betting Phase

--- src/round/implementation/DrawPokerRound.java
+++ src/round/implementation/DrawPokerRound.java
@@ -34,6 +34,7 @@ public class DrawPokerRound extends RoundTemplate {
     	// Loop through players, tell UI if player can open or not
     	setOrder(); //This is the order for the CURRENT round.
     	beginBettingPhase();	//controls betting phase.
+    	getWinner();
     }
 
     @Override
@@ -96,32 +97,64 @@ public class DrawPokerRound extends RoundTemplate {
     }
     
     @Override
-    protected void beginBettingPhase(){   	
-    	ListIterator<String> listIterator = roundOrder.listIterator();
-    	String startingPlayer = roundOrder.getFirst();	//first player must make bet
-    	Player start = players.get(startingPlayer);
-    	int currentBet = start.getOpeningBet();
-    	//boolean decisionChecker = false;
-    	//while (decisionChecker != true){
-    	//decisionChecker = true;
-	    	while (listIterator.hasNext()){
-	    		String playerName = listIterator.next();
-	    		Player p = players.get(playerName);
-	    		if (isFolding(playerName) == false){
-	    			if(p.isFolding() == true){
-	    				//remove from linkedlist
-	    				//set isFolding(playerName) true);	    				
-	    			} else if (p.isCalling() == true){
-	    				//subtract value from player bank account.
-	    				//add value to pot.
-	    				//break
-	    			} else if (p.isRaising() == true){
+    protected void beginBettingPhase(){ 
+		boolean allCalled = true;
+		ListIterator<String> listIterator = roundOrder.listIterator();
+		String startingPlayer = roundOrder.getFirst();	//first player must make bet
+		Player start = players.get(startingPlayer);
+		int currentBet = start.getOpeningBet();
+		listIterator.next();							//skip first player as already made bet.
+		while (roundOrder.size() > 1){					//while there is more than one player still playing loop
+			allCalled = true;
+			while (listIterator.hasNext()){				//Loops through roundOrder until reaches last player.
+		    	String playerName = listIterator.next();
+		    	Player p = players.get(playerName);
+		     		if(p.isFolding()){
+		    			roundOrder.remove(p);		//remove from linkedlist as out of round.
+		    		} else if (p.isCalling()){
+		    			bank.withdraw(playerName, currentBet);	//subtract value from player bank account.
+		    			pot.addChips(playerName, currentBet);	//add value to pot.
+		    		} else if (p.isRaising()){
 	    				currentBet = p.getRaise();
-	    				//break. Must raise if you reach this point.
-	    				//everyone call set to false apart from player raising.
-	    			}
+	    				bank.withdraw(playerName, currentBet);	//subtract value from player bank account.
+	    				pot.addChips(playerName, currentBet);
+	    				allCalled = false;						//allCall set to false as player has raised.
+	    				//reOrder round
+	    				reOrder(p);	//Reorders players, now person who raise is first.
+	    				break;
+		    	}
+		    }
+		    //Check to see if everyone has called.
+		    if (allCalled == true){
+		    	break;	//If all have called then break from while loop. Betting Phase over.
+			    }
 	    	}
-    	}
+		
+	}
+    
+    private void reOrder(Player playerWhoRaised){						//Reorder function for when a player raises.
+		String firstPlayer = playerWhoRaised.toString();
+	    int remainder = 0;
+	    boolean firstFound = false;
+	    LinkedList<String> tempReorder = new LinkedList<String>();
+	    tempReorder = (LinkedList<String>) roundOrder.clone();
+	    roundOrder.clear();
+	    ListIterator<String> listIterator = tempReorder.listIterator();
+	    while(listIterator.hasNext()){
+	    		String p = listIterator.next();
+				if (p.equals(firstPlayer)){
+					firstFound = true;
+					roundOrder.addFirst(p);
+					remainder = 1;
+					continue;
+				}
+				if (firstFound){
+					roundOrder.add(remainder, p);
+					remainder++;
+				}else{
+					roundOrder.addLast(p);
+				}
+			}
     }
     
     @Override
@@ -131,18 +164,39 @@ public class DrawPokerRound extends RoundTemplate {
     
     @Override
     public int getCallValue(){
-        //Ciarain implement!
+        //Ciaran implement?
         return 0;
     }
 
     @Override
-    public String getWinner(){    
+    public String getWinner(){
+    	//Loop through remaining players in roundOrder. Compare hand values.
+    	Player winner = null;
+    	Player temp = null;
+    	int handOfWinner = 0;
+    	ListIterator<String> listIterator = roundOrder.listIterator();
+    	while (listIterator.hasNext()){
+    		String playerName = listIterator.next();
+    		Player p = players.get(playerName);
+    		int handOfTemp = p.getHand().getGameValue();
+    		if (handOfTemp > handOfWinner){
+    			winner = p;
+    		}
+    		handOfWinner = winner.getHand().getGameValue();
+    	}
+    	String winnerName = winner.toString();
+    	addWinnings(winnerName, 0);
         return super.winner;
     }
 
     @Override
     protected void addWinnings(String winner, int potSplit){
-        
+        int winnings = pot.getTotalValue();
+        for (String p: players.keySet()){
+        	if (p.equals(winner)){
+        		bank.deposit(p, winnings);
+        	}
+        }
     }
 }
 //test

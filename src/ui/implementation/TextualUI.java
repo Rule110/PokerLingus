29c63d3dcba16e4bb8da2e475bad7db5a4d0f2d5
Cameron Newman <cameron.newman@ucdconnect.ie>
2017-04-29 12:59:14 +0100

    TUI Decide Strategy Implemented

--- src/ui/implementation/TextualUI.java
+++ src/ui/implementation/TextualUI.java
@@ -1,22 +1,16 @@
 package ui.implementation;
 
-import java.io.PrintStream;
-import java.util.Scanner;
-
 import game.framework.Game;
 import hand.framework.Hand;
 import network.framework.Network;
 import network.implementation.LocalNetwork;
 import round.framework.Round;
-import textupdate.framework.TextUpdate;
 import textupdate.implementation.GameStateTextUpdate;
 
 public class TextualUI extends UITemplate {
     
-	Network network = new LocalNetwork("testPlayer");
-	PrintStream outStream = System.out;
-	Scanner input = new Scanner(System.in);
-	GameStateTextUpdate message;
+	private Network network = new LocalNetwork("testPlayer");
+	private GameStateTextUpdate message;
 	
     public TextualUI(Game game){
         super(game);
@@ -24,93 +18,96 @@ public class TextualUI extends UITemplate {
     }
     
     public void decideStrategy(Hand hand, Round round){
-        if(isFolding()){
-        	
-        } else if(isRaising()){
-        	
-        } else if(isCalling()){
-        	
-        }
-    }
-    
-    public void intro(){
-    	message.setText("Welcome to the Automated Poker Machine... \nLet's Play Poker!");
-    	network.sendTextUpdate(message);
-    }
-    
-    public boolean isFolding(){
-    	String fold;
+    	boolean strategy = false;
     	
-    	while(isFolding != true || isFolding != false){
-    		message.setText("Would you like to fold (y/n)?: ");
-        	network.sendTextUpdate(message);
-        	fold = network.getMessageUpdate();
-        	
-        	switch (fold.toLowerCase()){
-	    		case "y":
-	    			isFolding = true;
-	    			break;
-	    		case "n":
-	    			isFolding = false;
-	    			break;
-	    		default:
-	    			message.setText("Please enter a valid character!");
-	            	network.sendTextUpdate(message);
-	    			break;    	
-        	}
+    	while(!strategy){
+	        if(!setFolding()){
+		        if(!setRaising()){
+		        	if(setCalling()){
+		        		strategy = true;
+		        	}
+		        } else{
+		        	setRaise();
+		        	strategy = true;
+		        }
+	        } else
+	        	strategy = true;
+    	}
+    }
+    
+    public boolean setFolding(){
+    	String fold;
+
+		message.setText("Would you like to fold (y/n)?: ");
+    	network.sendTextUpdate(message);
+    	fold = network.getMessageUpdate();
+    	
+    	switch (fold.toLowerCase()){
+    		case "y":
+    			isFolding = true;
+    			break;
+    		case "n":
+    			isFolding = false;
+    			break;
+    		default:
+    			message.setText("Please enter a valid character!");
+            	network.sendTextUpdate(message);
+            	message.setText("Would you like to fold (y/n)?: ");
+            	network.sendTextUpdate(message);
+            	fold = network.getMessageUpdate();   	
     	}
     	return isFolding;
     }
     
-    public boolean isCalling(){
+    public boolean setCalling(){
     	String call;
+
+		message.setText("Would you like to call (y/n)?: ");
+    	network.sendTextUpdate(message);
+    	call = network.getMessageUpdate();
     	
-    	while(isCalling != true || isCalling != false){
-    		message.setText("Would you like to call (y/n)?: ");
-        	network.sendTextUpdate(message);
-        	call = network.getMessageUpdate();
-        	
-        	switch (call.toLowerCase()){
-	    		case "y":
-	    			isCalling = true;
-	    			break;
-	    		case "n":
-	    			isCalling = false;
-	    			break;
-	    		default:
-	    			message.setText("Please enter a valid character!");
-	            	network.sendTextUpdate(message);
-	    			break;    	
-        	}
+    	switch (call.toLowerCase()){
+    		case "y":
+    			isCalling = true;
+    			break;
+    		case "n":
+    			isCalling = false;
+    			break;
+    		default:
+    			message.setText("Please enter a valid character!");
+            	network.sendTextUpdate(message);
+            	message.setText("Would you like to call (y/n)?: ");
+            	network.sendTextUpdate(message);
+            	call = network.getMessageUpdate();   	
     	}
     	return isCalling;
     }
     
-    public boolean isRaising(){
+    public boolean setRaising(){
     	String raise;
+
+		message.setText("Would you like to raise (y/n)?: ");
+    	network.sendTextUpdate(message);
+    	raise = network.getMessageUpdate();
     	
-    	while(isRaising != true || isRaising != false){
-    		message.setText("Would you like to raise (y/n)?: ");
-        	network.sendTextUpdate(message);
-        	raise = network.getMessageUpdate();
-        	
-        	switch (raise.toLowerCase()){
-	    		case "y":
-	    			isRaising = true;
-	    			break;
-	    		case "n":
-	    			isRaising = false;
-	    			break;
-	    		default:
-	    			message.setText("Please enter a valid character!");
-	            	network.sendTextUpdate(message);
-	    			break;    	
-        	}
+    	switch (raise.toLowerCase()){
+    		case "y":
+    			isRaising = true;
+    			break;
+    		case "n":
+    			isRaising = false;
+    			break;
+    		default:
+    			message.setText("Please enter a valid character!");
+            	network.sendTextUpdate(message);
+            	message.setText("Would you like to raise (y/n)?: ");
+            	network.sendTextUpdate(message);
+            	raise = network.getMessageUpdate();   	
     	}
     	return isRaising;
     }
     
-    public int getRaise(){
+    public int setRaise(){
         int playerChips = 0;
     	Boolean validAmount = false;
     	

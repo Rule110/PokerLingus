87cf3e7ab57c6bb52b5ab7d6267fbe37f6278a01
DKTFAY <darraghfay@hotmail.com>
2017-04-30 21:42:31 +0100

    Possibly fixed twitter update crisis.

--- src/pokerfaice/PokerFAIce.java
+++ src/pokerfaice/PokerFAIce.java
@@ -8,6 +8,9 @@ import game.framework.GameFactory;
 import network.framework.Network;
 import network.framework.NetworkFactory;
 import network.implementation.TwitterStreamListener;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
 import twitter4j.TwitterStream;
 import twitter4j.TwitterStreamFactory;
 import twitter4j.auth.AccessToken;
@@ -27,6 +30,7 @@ public class PokerFAIce {
     private static final String ACCESS_TOKEN = "850829181332189184-ggJdPa7miGGsnJeDexGIjGs6qcmOprt";
     private static final String ACCESS_TOKEN_SECRET = "HIYDWwSWuoraBXYW9YjrERCFe06zp9bX7maDm8KwCUosO";
     
+    private static Twitter twitter;
     volatile boolean hasCreationQueued = false;
     /**
      * Main method for the PokerFAIce Twitter Bot
@@ -41,10 +45,13 @@ public class PokerFAIce {
     public static void main(String[] args){
     	PokerFAIce pf = new PokerFAIce();
         TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+        twitter = new TwitterFactory().getInstance();
         
         twitterStream.setOAuthConsumer(CONSUMER_KEY, CONSUMER_KEY_SECRET);
+        twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_KEY_SECRET);
 		AccessToken accessToken = new AccessToken(ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
 		twitterStream.setOAuthAccessToken(accessToken);
+		twitter.setOAuthAccessToken(accessToken);
 		
         TwitterStreamListener listener = new TwitterStreamListener(pf);
         twitterStream.addListener(listener);
@@ -68,5 +75,14 @@ public class PokerFAIce {
     
     public Game getGame(String userHandle){
     	return games.get(userHandle);
+    }
+    
+    public void deleteMessage(long messageId){
+    	try {
+			twitter.destroyDirectMessage(messageId);
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
 }
